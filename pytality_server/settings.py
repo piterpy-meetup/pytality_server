cb4537d07fc278a4d0eccc67b8a8c46db213bf02
Dmitry Nazarov <mail@nazarov.tech>
2018-08-18 18:28:29 +0300

    Fix broken admin display

--- pytality_server/settings.py
+++ pytality_server/settings.py
@@ -92,6 +92,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # ASGI, channels
 ASGI_APPLICATION = "pytality_server.routing.application"
--- pytality_server/urls.py
+++ pytality_server/urls.py
@@ -1,6 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()

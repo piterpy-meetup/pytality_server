396ec994ed4b6bd30e4e0f45fef50c7fb7fee2f0
Dmitry Nazarov <mail@nazarov.tech>
2018-08-18 16:06:07 +0300

    Improve admin list

--- pytality_server/api/admin.py
+++ pytality_server/api/admin.py
@@ -4,7 +4,7 @@ from pytality_server.api.models import Snippet
 
 
 class SnippetAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('task_title', 'time_to_solve',)
 
 
 admin.site.register(Snippet, SnippetAdmin)
